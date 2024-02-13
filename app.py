03ba5ce1a3ea801bc19b7d5f142e6eb88be3e843
Entrepreneurrichpj <59737655+Entrepreneurrichpj@users.noreply.github.com>
2024-02-13 15:52:26 -0500

    Adjust for desktop application

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import tkinter as tk
 from tkinter import scrolledtext
 from PIL import Image, ImageTk
@@ -8,22 +10,41 @@ thread_id = ''
 
 def fetch_gpt_response(user_query):
     global thread_id
-    gpt_json = gpt.generate_mail(user_query)
+    gpt_response.config(state=tk.NORMAL)
+    gpt_json = gpt.generate_mail(user_query, gpt_response)
+    gpt_response.config(state=tk.DISABLED)
+    if gpt_json is None:
+        print_error_message()
+        return
     thread_id = gpt_json['thread_id']
     response = gpt_json['messages'].data[0].content[0].text.value
     root.after(0, lambda: update_gpt_response(response))
 
+def print_error_message():
+    gpt_response.config(state=tk.NORMAL)
+    gpt_response.delete(1.0, tk.END)
+    gpt_response.insert(tk.END, "Something's wrong with the server, please try again later...\n服务器出问题了，请稍后重试...")    
+    gpt_response.config(state=tk.DISABLED)
+
 def refresh_gpt_response():
-    gpt_response.insert(tk.END, "Regenerating the response...\n正在重新生成邮件，请稍等...")
-    response = gpt.regenerate(thread_id)
+    gpt_response.config(state=tk.NORMAL)
+    gpt_response.insert(tk.END, "\nRegenerating the response...\n正在重新生成邮件，请稍等...")
+    response = gpt.regenerate(thread_id, gpt_response)
+    gpt_response.config(state=tk.DISABLED)
+    if response is None:
+        print_error_message()
+        return
     root.after(0, lambda: update_gpt_response(response))
 
 def update_gpt_response(response):
+    gpt_response.config(state=tk.NORMAL)
     gpt_response.delete(1.0, tk.END)
     gpt_response.insert(tk.END, response)
+    gpt_response.config(state=tk.DISABLED)
     update_refresh_button()
 
 def submit_query():
+    gpt_response.config(state=tk.NORMAL)
     if to_entry.get() == '':
         gpt_response.delete(1.0, tk.END)
         gpt_response.insert(tk.END, "Please tell me who is this for...\n请告诉这是写给谁的...")    
@@ -41,6 +62,7 @@ def submit_query():
     gpt_response.delete(1.0, tk.END)
     gpt_response.insert(tk.END, "Got it, please wait while I'm generating the response...\n正在生成邮件，请稍等...")
     user_input.delete(0, tk.END)
+    gpt_response.config(state=tk.DISABLED)
     threading.Thread(target=fetch_gpt_response, args=(user_query,)).start()
 
 def update_refresh_button():
@@ -54,6 +76,18 @@ def resize_icon(path, new_width, new_height):
     resized_image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
     return ImageTk.PhotoImage(resized_image)
 
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller."""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+
 root = tk.Tk()
 root.title("Tell me what to send")
 
@@ -63,8 +97,11 @@ root.minsize(400, 300)
 root.bind('<Return>', lambda event=None: submit_query())
 
 # LOAD ICONS
-refresh_icon = resize_icon('./Images/refresh.png', 24, 24)
-submit_icon = resize_icon('./Images/enter.png', 24, 24)
+refresh_icon_path = resource_path(os.path.join("Images", "refresh.png"))
+refresh_icon = resize_icon(refresh_icon_path, 24, 24)
+
+submit_icon_path = resource_path(os.path.join("Images", "enter.png"))
+submit_icon = resize_icon(submit_icon_path, 24, 24)
 
 # 'To' AND 'From' FIELDS
 tk.Label(root, text="To:").grid(row=0, column=0, sticky='w', padx=5, pady=5)
@@ -79,6 +116,7 @@ from_entry.grid(row=1, column=1, sticky='ew', padx=5, pady=5)
 gpt_response = scrolledtext.ScrolledText(root, wrap=tk.WORD, height=10)
 gpt_response.grid(row=2, column=0, columnspan=3, sticky='nsew', padx=5, pady=5)
 gpt_response.insert(tk.END, 'I will tell you what to send...\n我会告诉你怎么写...')
+gpt_response.config(state=tk.DISABLED)
 
 # Refresh button for GPT text field
 refresh_button = tk.Button(root, image=refresh_icon, borderwidth=0, highlightthickness=0, bg='white', command=lambda: threading.Thread(target=refresh_gpt_response).start())
--- gpt.py
+++ gpt.py
@@ -1,48 +1,70 @@
+import sys
+import tkinter
 import openai, dotenv, os, time
 
-dotenv.load_dotenv()
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+dotenv.load_dotenv(resource_path('.env'))
 
 client = openai.OpenAI(api_key=os.getenv('OPEN_AI_API'))
 
-def generate_mail(prompt):
-    assistant = client.beta.assistants.retrieve(assistant_id=os.getenv('ASSISTANT_ID'))
-    thread = client.beta.threads.create()
-    client.beta.threads.messages.create(
-        thread_id=thread.id,
-        role='user',
-        content=prompt
-    )
-    run = client.beta.threads.runs.create(
-        thread_id=thread.id,
-        assistant_id=assistant.id
-    )
-    while run.status != 'completed':
-        run = client.beta.threads.runs.retrieve(
-            thread_id = thread.id,
-            run_id = run.id
-        )
-        time.sleep(0.5)
-    messsages = client.beta.threads.messages.list(thread_id=thread.id)
-    response = {"thread_id":thread.id, "messages":messsages}
-    return response
 
-def regenerate(thread_id):
-    assistant = client.beta.assistants.retrieve(assistant_id=os.getenv('ASSISTANT_ID'))
-    client.beta.threads.messages.create(
-        thread_id=thread_id,
-        role='user',
-        content="can you regenerate this email in a different style?"
-    )
-    run = client.beta.threads.runs.create(
-        thread_id=thread_id,
-        assistant_id=assistant.id
-    )
-    while run.status != 'completed':
-        run = client.beta.threads.runs.retrieve(
-            thread_id = thread_id,
-            run_id = run.id
+
+def generate_mail(prompt, gpt_response):
+    try:
+        assistant = client.beta.assistants.retrieve(assistant_id=os.getenv('ASSISTANT_ID'))
+        print(assistant.id)
+        thread = client.beta.threads.create()
+        client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role='user',
+            content=prompt
         )
-        time.sleep(0.5)
-    messsages = client.beta.threads.messages.list(thread_id=thread_id)
-    response = messsages.data[0].content[0].text.value
-    return response
+        run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=assistant.id
+        )
+        while run.status != 'completed':
+            run = client.beta.threads.runs.retrieve(
+                thread_id = thread.id,
+                run_id = run.id
+            )
+            gpt_response.insert(tkinter.END, f'\n{run.status}')
+            time.sleep(0.5)
+        messsages = client.beta.threads.messages.list(thread_id=thread.id)
+        response = {"thread_id":thread.id, "messages":messsages}
+        return response
+    except openai.OpenAIError as e:
+        print(e)
+        return None
+
+
+def regenerate(thread_id, gpt_response):
+    try:
+        assistant = client.beta.assistants.retrieve(assistant_id=os.getenv('ASSISTANT_ID'))
+        client.beta.threads.messages.create(
+            thread_id=thread_id,
+            role='user',
+            content="can you regenerate this email in a different style?"
+        )
+        run = client.beta.threads.runs.create(
+            thread_id=thread_id,
+            assistant_id=assistant.id
+        )
+        while run.status != 'completed':
+            run = client.beta.threads.runs.retrieve(
+                thread_id = thread_id,
+                run_id = run.id
+            )
+            gpt_response.insert(tkinter.END, f'\n{run.status}')
+            time.sleep(0.5)
+        messsages = client.beta.threads.messages.list(thread_id=thread_id)
+        response = messsages.data[0].content[0].text.value
+        return response
+    except openai.OpenAIError as e:
+        return None
